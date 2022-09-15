10e46c0e2473ef486167db8aa6669d4af8399368
hardikphalet <hardik.phalet@gmail.com>
2022-09-15 20:33:44 +0530

    Added Generate functionality Using region-service repository as skeleton for now. Hydrate functionality isn't fully functional.

--- src/main/java/com/trips/create_service/tasks/Generate.java
+++ src/main/java/com/trips/create_service/tasks/Generate.java
@@ -1,7 +1,27 @@
 package com.trips.create_service.tasks;
 
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+
+import java.nio.file.Paths;
+
 public class Generate {
     public static void execute() {
-        System.out.println("generate command used.");
+        String repoLink = "https://" + "github.com/hardikphalet/region-service.git";
+        String cloneDirectoryPath = "./skeleton";
+
+        try {
+            System.out.println("Cloning "+repoLink+" into "+cloneDirectoryPath);
+            Git.cloneRepository()
+                    .setURI(repoLink)
+                    .setDirectory(Paths.get(cloneDirectoryPath).toFile())
+                    .call();
+            System.out.println("Completed Cloning");
+        } catch (GitAPIException e) {
+            System.out.println("Exception occurred while cloning the skeleton repository.");
+        }
+
+
+        System.out.println("Skeleton repository cloned successfully.");
     }
 }
--- src/main/java/com/trips/create_service/tasks/Hydrate.java
+++ src/main/java/com/trips/create_service/tasks/Hydrate.java
@@ -1,7 +1,29 @@
 package com.trips.create_service.tasks;
 
+import java.io.File;
+
 public class Hydrate {
     public static void execute() {
+        File folder = new File("./skeleton/src/main/java/com/cf/serve/entities");
+        File[] entityList = folder.listFiles();
+        if (!folder.isDirectory()) {
+            System.out.println("Entity package doesn't exist. Please re-run the -generate command.");
+            return;
+        }
+
+        if(entityList == null || entityList.length == 0) {
+            System.out.println("No entities.");
+            return;
+        }
+
+        StringBuilder stringBuilder = new StringBuilder("Entites found are: ");
+        for (File entity : entityList) {
+            stringBuilder.append(entity.getName().split("[.]")[0]);
+            stringBuilder.append(" | ");
+        }
+
+        System.out.println(stringBuilder);
+
         System.out.println("hydrate command used.");
     }
 }
